4d6bac338297298cfed5982cc7322bfc17b66013
Miles Raker <milesraker@gmail.com>
2022-12-07 12:39:47 -0800

    Aliases exercises complete

---
+++ aliases_exercises.sql
@@ -0,0 +1,4 @@
+USE employees;
+SELECT CONCAT(last_name, ', ', first_name) FROM employees LIMIT 10;
+SELECT CONCAT(last_name, ', ', first_name), birth_date as DOB FROM employees LIMIT 10;
+SELECT CONCAT(emp_no, '- ', last_name, ', ', first_name) as full_name, birth_date as DOB FROM employees LIMIT 10;
