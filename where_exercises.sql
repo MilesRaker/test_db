6945c976c92949a6de30c796d8d4915c8d51f879
Miles Raker <milesraker@gmail.com>
2022-12-07 10:28:54 -0800

    WHERE part 2 Complete

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,4 +1,12 @@
 USE employees;
+/* Part 1 */
 SELECT first_name FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya');
 SELECT last_name FROM employees WHERE last_name LIKE 'E%';
 SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%';
+
+/* Part 2 */
+SELECT first_name, last_name FROM employees WHERE first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya';
+SELECT first_name, last_name FROM employees WHERE gender = 'M' AND (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya');
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%e' OR last_name LIKE 'e%';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%e' AND last_name LIKE 'e%';
+SELECT first_name, last_name FROM employees WHERE last_name LIKE '%q%' AND NOT last_name LIKE '%qu%';
